30f321f1ae87f69715d8fed0608ea2444cd28122
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-29 06:32:55 +0530

    refactor: nest tests inside an inner class

--- core/src/test/kotlin/io/redgreen/cardbox/AssociationTest.kt
+++ core/src/test/kotlin/io/redgreen/cardbox/AssociationTest.kt
@@ -5,45 +5,49 @@ import io.redgreen.cardbox.GetClassesRootDirectoryUseCase.Association
 import io.redgreen.cardbox.PackageNameFromClassUseCase.Result.DefaultPackage
 import io.redgreen.cardbox.PackageNameFromClassUseCase.Result.PackageName
 import java.io.File
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class AssociationTest {
-  @Test
-  fun `test association to jar tool`() {
-    // given
-    val testClassesDirectory = File("./build/classes/kotlin/test/io/redgreen/cardbox")
-    val association = Association(
-      testClassesDirectory,
-      PackageName("io.redgreen.cardbox")
-    )
+  @Nested
+  inner class JarToolPaths {
+    @Test
+    fun `test classes`() {
+      // given
+      val testClassesDirectory = File("./build/classes/kotlin/test/io/redgreen/cardbox")
+      val association = Association(
+        testClassesDirectory,
+        PackageName("io.redgreen.cardbox")
+      )
 
-    // when & then
-    assertThat(association.jarToolPath)
-      .isEqualTo(File("./build/classes/kotlin/test/io"))
-  }
+      // when & then
+      assertThat(association.jarToolPath)
+        .isEqualTo(File("./build/classes/kotlin/test/io"))
+    }
 
-  @Test
-  fun `production association to jar tool path`() {
-    // given
-    val productionClassesDirectory = File("./build/classes/kotlin/main/io/redgreen/cardbox")
-    val association = Association(
-      productionClassesDirectory,
-      PackageName("io.redgreen.cardbox")
-    )
+    @Test
+    fun `production classes`() {
+      // given
+      val productionClassesDirectory = File("./build/classes/kotlin/main/io/redgreen/cardbox")
+      val association = Association(
+        productionClassesDirectory,
+        PackageName("io.redgreen.cardbox")
+      )
 
-    // when & then
-    assertThat(association.jarToolPath)
-      .isEqualTo(File("./build/classes/kotlin/main/io"))
-  }
+      // when & then
+      assertThat(association.jarToolPath)
+        .isEqualTo(File("./build/classes/kotlin/main/io"))
+    }
 
-  @Test
-  fun `default package to jar tool path`() {
-    // given
-    val classesDirectory = File("./build/classes/java/test")
-    val association = Association(classesDirectory, DefaultPackage)
+    @Test
+    fun `default package`() {
+      // given
+      val classesDirectory = File("./build/classes/java/test")
+      val association = Association(classesDirectory, DefaultPackage)
 
-    // when & then
-    assertThat(association.jarToolPath)
-      .isEqualTo(File("./build/classes/java/test"))
+      // when & then
+      assertThat(association.jarToolPath)
+        .isEqualTo(File("./build/classes/java/test"))
+    }
   }
 }
