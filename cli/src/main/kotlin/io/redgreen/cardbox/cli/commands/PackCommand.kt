6f59f990a239f5c04cae000bc04bdf75b2a562cb
Ragunath Jawahar <ragunath@redgreen.io>
2021-07-02 17:44:24 +0530

    refactor: extract functions

--- cli/src/main/kotlin/io/redgreen/cardbox/cli/commands/PackCommand.kt
+++ cli/src/main/kotlin/io/redgreen/cardbox/cli/commands/PackCommand.kt
@@ -1,7 +1,10 @@
 package io.redgreen.cardbox.cli.commands
 
 import io.redgreen.cardbox.DiscoverPotentialArtifactsUseCase
+import io.redgreen.cardbox.model.ArtifactName
 import io.redgreen.cardbox.model.JarToolShellCommand
+import io.redgreen.cardbox.model.PackagesInPath
+import io.redgreen.cardbox.model.SourceSet
 import io.redgreen.cardbox.model.SourceSet.UNDETERMINED
 import java.io.File
 import picocli.CommandLine.Command
@@ -16,6 +19,7 @@ class PackCommand : Runnable {
   companion object {
     private const val USER_HOME_KEY = "user.home"
     private const val ARTIFACT_DIRECTORY_NAME = "cardbox"
+    private const val EMOJI_PACKAGE = "\uD83D\uDCE6"
   }
 
   @Parameters(index = "0", description = ["directory"])
@@ -30,25 +34,39 @@ class PackCommand : Runnable {
     val sourceSetsArtifacts = DiscoverPotentialArtifactsUseCase()
       .invoke(directory)
       .toMutableMap()
-
     sourceSetsArtifacts.remove(UNDETERMINED)
 
+    if (sourceSetsArtifacts.values.isNotEmpty()) {
+      if (!outputDirectory.exists()) {
+        outputDirectory.mkdirs()
+      }
+    }
+
+    createArtifacts(sourceSetsArtifacts)
+  }
+
+  private fun createArtifacts(
+    sourceSetsArtifacts: MutableMap<SourceSet, Map<ArtifactName, List<PackagesInPath>>>
+  ) {
     sourceSetsArtifacts.onEach { (sourceSet, artifactNamesPackagesInPathMap) ->
       println(sourceSet)
       println("==========")
-
       artifactNamesPackagesInPathMap.onEach { (artifactName, packagesInPath) ->
-        println(artifactName.value)
-        if (outputDirectory.exists().not()) {
-          outputDirectory.mkdirs()
-        }
-        val jarShellCommand = JarToolShellCommand.from(artifactName, packagesInPath, outputDirectory)
-        println(jarShellCommand.text())
-
-        val processBuilder = ProcessBuilder(jarShellCommand.program, *jarShellCommand.arguments.toTypedArray())
-        val process = processBuilder.start()
-        println(process.errorStream.bufferedReader().readText())
+        println("$EMOJI_PACKAGE ${artifactName.value}")
+        executeJarCommand(artifactName, packagesInPath)
       }
     }
+  }
+
+  private fun executeJarCommand(
+    artifactName: ArtifactName,
+    packagesInPath: List<PackagesInPath>
+  ) {
+    val jarShellCommand = JarToolShellCommand.from(artifactName, packagesInPath, outputDirectory)
+    println(jarShellCommand.text())
+
+    val processBuilder = ProcessBuilder(jarShellCommand.program, *jarShellCommand.arguments.toTypedArray())
+    val process = processBuilder.start()
+    println(process.errorStream.bufferedReader().readText())
   }
 }
