490190ad4b378fb859c174d138a1b5b586747efe
Ragunath Jawahar <ragunath@redgreen.io>
2021-07-02 17:22:13 +0530

    feat: store artifacts in user's home directory

--- cli/src/main/kotlin/io/redgreen/cardbox/cli/commands/PackCommand.kt
+++ cli/src/main/kotlin/io/redgreen/cardbox/cli/commands/PackCommand.kt
@@ -13,8 +13,18 @@ import picocli.CommandLine.Parameters
   description = ["runs the JDK jar tool and produces .jar artifacts"]
 )
 class PackCommand : Runnable {
+  companion object {
+    private const val USER_HOME_KEY = "user.home"
+    private const val ARTIFACT_DIRECTORY_NAME = "cardbox"
+  }
+
   @Parameters(index = "0", description = ["directory"])
   lateinit var directory: File
+
+  private val outputDirectory: File by lazy {
+    val projectName = directory.canonicalFile.name
+    File(System.getProperty(USER_HOME_KEY)).resolve(ARTIFACT_DIRECTORY_NAME).resolve(projectName)
+  }
 
   override fun run() {
     val sourceSetsArtifacts = DiscoverPotentialArtifactsUseCase()
@@ -29,15 +39,16 @@ class PackCommand : Runnable {
 
       artifactNamesPackagesInPathMap.onEach { (artifactName, packagesInPath) ->
         println(artifactName.value)
-        val jarShellCommand = JarToolShellCommand.from(artifactName, packagesInPath)
-        println(jarShellCommand)
+        if (outputDirectory.exists().not()) {
+          outputDirectory.mkdirs()
+        }
+        val jarShellCommand = JarToolShellCommand.from(artifactName, packagesInPath, outputDirectory)
+        println(jarShellCommand.text())
 
         val processBuilder = ProcessBuilder(jarShellCommand.program, *jarShellCommand.arguments.toTypedArray())
         val process = processBuilder.start()
         println(process.errorStream.bufferedReader().readText())
       }
-      println()
     }
-    println()
   }
 }
