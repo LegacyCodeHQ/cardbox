5dff6871f7451be815465f7b523860d50bc477a0
Ragunath Jawahar <ragunath@redgreen.io>
2023-03-18 12:59:39 +0530

    feat: print version name from properties file

--- cli/src/main/kotlin/io/redgreen/cardbox/cli/CardboxCommand.kt
+++ cli/src/main/kotlin/io/redgreen/cardbox/cli/CardboxCommand.kt
@@ -3,15 +3,22 @@ package io.redgreen.cardbox.cli
 import io.redgreen.cardbox.cli.commands.DiscoverCommand
 import io.redgreen.cardbox.cli.commands.PackCommand
 import picocli.CommandLine.Command
-
-internal const val TOOL_VERSION = "0.1.0"
+import picocli.CommandLine.Option
 
 @Command(
   name = "cardbox",
-  mixinStandardHelpOptions = true,
-  subcommands = [DiscoverCommand::class, PackCommand::class],
-  description = ["Create JARs from Android projects."],
+  subcommands = [
+    DiscoverCommand::class,
+    PackCommand::class,
+  ],
+  description = ["Create JARs from Android projects for jQAssistant."],
   commandListHeading = "%nCommands:%n%nAvailable commands are:%n",
-  version = ["v$TOOL_VERSION"],
 )
-class CardboxCommand
+class CardboxCommand {
+  @Option(
+    names = ["--version", "-v"],
+    description = ["prints version"],
+    versionHelp = true,
+  )
+  var version: Boolean = false
+}
--- cli/src/main/kotlin/io/redgreen/cardbox/cli/Cli.kt
+++ cli/src/main/kotlin/io/redgreen/cardbox/cli/Cli.kt
@@ -1,7 +1,23 @@
 package io.redgreen.cardbox.cli
 
+import java.util.Properties
+import kotlin.system.exitProcess
 import picocli.CommandLine
 
 fun main(args: Array<String>) {
-  CommandLine(CardboxCommand()).execute(*args)
+  val commandLine = CommandLine(CardboxCommand())
+  val exitCode = commandLine.execute(*args)
+
+  if (commandLine.isVersionHelpRequested) {
+    printVersion()
+    exitProcess(0)
+  }
+
+  exitProcess(exitCode)
+}
+
+fun printVersion() {
+  val properties = CardboxCommand::class.java.classLoader
+    .getResourceAsStream("version.properties").use { Properties().apply { load(it) } }
+  println(properties["version"])
 }
