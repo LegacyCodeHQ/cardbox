98c92b718339be8b8d12a97f9aa61e1d089b8245
Ragunath Jawahar <ragunath@redgreen.io>
2023-03-18 12:47:26 +0530

    build: configure jreleaser Gradle plugin

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,17 +1,26 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.util.Properties
+import org.jreleaser.model.Active
 
 plugins {
   kotlin("jvm")
   application
   id("io.gitlab.arturbosch.detekt")
   id("com.github.johnrengelman.shadow") version "7.0.0"
+  id("org.jreleaser") version "1.2.0"
 }
 
 repositories {
   mavenCentral()
 }
 
-application.mainClass.set("io.redgreen.cardbox.cli.CliKt")
+val mainClassName = "io.redgreen.cardbox.cli.CliKt"
+val toolVersion = "0.1.0"
+
+application {
+  applicationName = "cardbox"
+  mainClass.set(mainClassName)
+}
 
 dependencies {
   implementation(project(":core"))
@@ -36,13 +45,21 @@ dependencies {
   testImplementation("com.google.truth:truth:1.1.3")
 }
 
+tasks.withType<Jar> {
+  manifest {
+    attributes(
+      "Main-Class" to mainClassName,
+    )
+  }
+}
+
 tasks {
   named<ShadowJar>("shadowJar") {
     archiveBaseName.set("cardbox")
     mergeServiceFiles()
 
     manifest {
-      attributes(mapOf("Main-Class" to "io.redgreen.cardbox.cli.CliKt"))
+      attributes(mapOf("Main-Class" to mainClassName))
     }
   }
 }
@@ -50,6 +67,27 @@ tasks {
 tasks {
   build {
     dependsOn(shadowJar)
+  }
+}
+
+tasks {
+  named("classes") {
+    dependsOn("createVersionFile")
+  }
+
+  create("createVersionFile") {
+    dependsOn(processResources)
+    doLast {
+      val resourcesDirectory = File("$buildDir/resources/main/")
+      if (!resourcesDirectory.exists()) {
+        resourcesDirectory.mkdirs()
+      }
+      resourcesDirectory.resolve("version.properties").bufferedWriter().use { writer ->
+        val properties = Properties()
+        properties["version"] = toolVersion
+        properties.store(writer, null)
+      }
+    }
   }
 }
 
@@ -66,6 +104,58 @@ tasks.register("executable", DefaultTask::class) {
     execFile.outputStream().use {
       it.write("#!/bin/sh\n\nexec java -Xmx512m -jar \"\$0\" \"\$@\"\n\n".toByteArray())
       it.write(inputs.files.singleFile.readBytes())
+    }
+  }
+}
+
+jreleaser {
+  version = toolVersion
+  gitRootSearch.set(true)
+
+  project {
+    name.set("cardbox")
+    description.set("Create JARs from Android projects for jQAssistant.")
+    website.set("https://github.com/redgreenio/cardbox")
+    license.set("Apache-2.0")
+    copyright.set("2021-Present, Ragunath Jawahar")
+  }
+
+  release {
+    github {
+      repoOwner.set("redgreenio")
+      name.set("cardbox")
+      branch.set("main")
+      repoUrl.set("git@github.com:redgreenio/cardbox.git")
+
+      tagName.set("cardbox-$toolVersion")
+      releaseName.set("cardbox $toolVersion")
+      overwrite.set(true)
+
+      token.set(System.getenv("GITHUB_TOKEN"))
+    }
+  }
+
+  distributions {
+    create("cardbox") {
+      artifact {
+        path.set(File("build/distributions/cardbox-$toolVersion.zip"))
+      }
+
+      brew {
+        active.set(Active.RELEASE)
+        formulaName.set("cardbox")
+
+        repoTap {
+          repoOwner.set("redgreenio")
+          name.set("homebrew-tap")
+        }
+
+        dependencies {
+          dependency("openjdk@11")
+        }
+
+        extraProperties.put("skipJava", true)
+      }
     }
   }
 }
