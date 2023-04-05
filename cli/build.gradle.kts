3eae12e370c2b9aefdf66fad62da96c55b8d1781
Ragunath Jawahar <ragunath@redgreen.io>
2023-04-05 18:12:52 +0530

    build: prepare release

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 val mainClassFqn = "io.redgreen.cardbox.cli.CliKt"
-val toolVersion = "0.3.0-SNAPSHOT"
+val toolVersion = "0.3.0"
 
 application {
   applicationName = "cardbox"
