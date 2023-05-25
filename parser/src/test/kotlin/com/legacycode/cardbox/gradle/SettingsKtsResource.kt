5c96b435ad72ac99b474664c56d864ed5b5915fa
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-25 11:58:52 +0530

    refactor: introduce `SettingsGroovyResource`

--- parser/src/test/kotlin/com/legacycode/cardbox/gradle/SettingsKtsResource.kt
+++ parser/src/test/kotlin/com/legacycode/cardbox/gradle/SettingsKtsResource.kt
@@ -3,9 +3,21 @@ package com.legacycode.cardbox.gradle
 class SettingsKtsResource(private val filename: String) {
   val content: String
     get() {
+      val resourcePath = "settings/kts/$filename"
       return SettingsKtsResource::class.java
         .classLoader
-        .getResource("settings/kts/$filename")!!
+        .getResource(resourcePath)!!
+        .readText()
+    }
+}
+
+class SettingsGroovyResource(private val filename: String) {
+  val content: String
+    get() {
+      val resourcePath = "settings/groovy/$filename"
+      return SettingsGroovyResource::class.java
+        .classLoader
+        .getResource(resourcePath)!!
         .readText()
     }
 }
