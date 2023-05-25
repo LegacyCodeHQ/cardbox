bfc68bbd7b5f512381238e9940418ce4cdae6c1d
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-25 12:34:04 +0530

    refactor: rename `module` to `subproject`

--- parser/src/main/kotlin/com/legacycode/cardbox/gradle/Subproject.kt
+++ parser/src/main/kotlin/com/legacycode/cardbox/gradle/Subproject.kt
@@ -3,42 +3,42 @@ package com.legacycode.cardbox.gradle
 data class Subproject(val name: String, val path: String)
 
 fun extractSubprojects(content: String): List<Subproject> {
-  val modulePattern1 = Regex("""include\([\s\S]*?\)""")
-  val modulePattern2 = Regex("""include '(.+?)'""")
+  val subprojectPattern1 = Regex("""include\([\s\S]*?\)""")
+  val subprojectPattern2 = Regex("""include '(.+?)'""")
   val projectNamePattern = Regex("""project\(':(.+?)'\)\.name = '(.+?)'""")
   val projectDirPattern = Regex("""project\(':(.+?)'\)\.projectDir = file\('(.+?)'\)""")
 
-  val moduleNames = mutableMapOf<String, Subproject>()
+  val subprojectNames = mutableMapOf<String, Subproject>()
 
-  val includeMatches1 = modulePattern1.findAll(content)
+  val includeMatches1 = subprojectPattern1.findAll(content)
   includeMatches1.forEach { matchResult ->
-    val modules = matchResult.value
+    val subprojects = matchResult.value
       .removePrefix("include(")
       .removeSuffix(")")
       .split(",")
       .map { it.trim().removeSurrounding("\"").removePrefix(":") }
       .filter { it.isNotEmpty() }
 
-    modules.forEach { moduleName ->
-      val path = moduleName.replace(":", "/")
-      moduleNames[moduleName] = Subproject(moduleName, path)
+    subprojects.forEach { subprojectName ->
+      val path = subprojectName.replace(":", "/")
+      subprojectNames[subprojectName] = Subproject(subprojectName, path)
     }
   }
 
-  val includeMatches2 = modulePattern2.findAll(content)
+  val includeMatches2 = subprojectPattern2.findAll(content)
   includeMatches2.map { it.groupValues[1].removePrefix(":") }
     .filter { it.isNotEmpty() }
-    .forEach { moduleName ->
-      val path = moduleName.replace(":", "/")
-      moduleNames[moduleName] = Subproject(moduleName, path)
+    .forEach { subprojectName ->
+      val path = subprojectName.replace(":", "/")
+      subprojectNames[subprojectName] = Subproject(subprojectName, path)
     }
 
   val projectNameMatches = projectNamePattern.findAll(content)
   projectNameMatches.forEach { matchResult ->
     val originalName = matchResult.groupValues[1]
     val newName = matchResult.groupValues[2]
-    moduleNames[originalName]?.let {
-      moduleNames[originalName] = it.copy(name = newName)
+    subprojectNames[originalName]?.let {
+      subprojectNames[originalName] = it.copy(name = newName)
     }
   }
 
@@ -46,10 +46,10 @@ fun extractSubprojects(content: String): List<Subproject> {
   projectDirMatches.forEach { matchResult ->
     val originalName = matchResult.groupValues[1]
     val newPath = matchResult.groupValues[2]
-    moduleNames[originalName]?.let {
-      moduleNames[originalName] = it.copy(path = newPath)
+    subprojectNames[originalName]?.let {
+      subprojectNames[originalName] = it.copy(path = newPath)
     }
   }
 
-  return moduleNames.values.toList()
+  return subprojectNames.values.toList()
 }
