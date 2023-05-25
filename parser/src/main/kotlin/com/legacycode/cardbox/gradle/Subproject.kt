a4abbee4adf166294593fe22cf3d63300ba4f078
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-25 12:41:20 +0530

    refactor: decompose larger function into smaller ones

--- parser/src/main/kotlin/com/legacycode/cardbox/gradle/Subproject.kt
+++ parser/src/main/kotlin/com/legacycode/cardbox/gradle/Subproject.kt
@@ -3,12 +3,21 @@ package com.legacycode.cardbox.gradle
 data class Subproject(val name: String, val path: String)
 
 fun extractSubprojects(content: String): List<Subproject> {
+  val outSubprojectNames = mutableMapOf<String, Subproject>()
+
+  extractIncludes(content, outSubprojectNames)
+  updateProjectNames(content, outSubprojectNames)
+  updateProjectDirs(content, outSubprojectNames)
+
+  return outSubprojectNames.values.toList()
+}
+
+private fun extractIncludes(
+  content: String,
+  outSubprojectNames: MutableMap<String, Subproject>,
+) {
   val subprojectPattern1 = Regex("""include\([\s\S]*?\)""")
   val subprojectPattern2 = Regex("""include '(.+?)'""")
-  val projectNamePattern = Regex("""project\(':(.+?)'\)\.name = '(.+?)'""")
-  val projectDirPattern = Regex("""project\(':(.+?)'\)\.projectDir = file\('(.+?)'\)""")
-
-  val subprojectNames = mutableMapOf<String, Subproject>()
 
   val includeMatches1 = subprojectPattern1.findAll(content)
   includeMatches1.forEach { matchResult ->
@@ -21,7 +30,7 @@ fun extractSubprojects(content: String): List<Subproject> {
 
     subprojects.forEach { subprojectName ->
       val path = subprojectName.replace(":", "/")
-      subprojectNames[subprojectName] = Subproject(subprojectName, path)
+      outSubprojectNames[subprojectName] = Subproject(subprojectName, path)
     }
   }
 
@@ -30,26 +39,38 @@ fun extractSubprojects(content: String): List<Subproject> {
     .filter { it.isNotEmpty() }
     .forEach { subprojectName ->
       val path = subprojectName.replace(":", "/")
-      subprojectNames[subprojectName] = Subproject(subprojectName, path)
+      outSubprojectNames[subprojectName] = Subproject(subprojectName, path)
     }
+}
+
+private fun updateProjectNames(
+  content: String,
+  outSubprojectNames: MutableMap<String, Subproject>,
+) {
+  val projectNamePattern = Regex("""project\(':(.+?)'\)\.name = '(.+?)'""")
 
   val projectNameMatches = projectNamePattern.findAll(content)
   projectNameMatches.forEach { matchResult ->
     val originalName = matchResult.groupValues[1]
     val newName = matchResult.groupValues[2]
-    subprojectNames[originalName]?.let {
-      subprojectNames[originalName] = it.copy(name = newName)
+    outSubprojectNames[originalName]?.let {
+      outSubprojectNames[originalName] = it.copy(name = newName)
     }
   }
+}
+
+private fun updateProjectDirs(
+  content: String,
+  outSubprojectNames: MutableMap<String, Subproject>,
+) {
+  val projectDirPattern = Regex("""project\(':(.+?)'\)\.projectDir = file\('(.+?)'\)""")
 
   val projectDirMatches = projectDirPattern.findAll(content)
   projectDirMatches.forEach { matchResult ->
     val originalName = matchResult.groupValues[1]
     val newPath = matchResult.groupValues[2]
-    subprojectNames[originalName]?.let {
-      subprojectNames[originalName] = it.copy(path = newPath)
+    outSubprojectNames[originalName]?.let {
+      outSubprojectNames[originalName] = it.copy(path = newPath)
     }
   }
-
-  return subprojectNames.values.toList()
 }
